002232a94cb112c9b6a97f3777be3cc532b9c994
Johannes Heinen <johannes.heinen@gmail.com>
2013-10-31 14:36:33 +0100

    Added DIC dependency

--- src/Phpforce/Bundle/SoapClientBundle/DependencyInjection/PhpforceSoapClientExtension.php
+++ src/Phpforce/Bundle/SoapClientBundle/DependencyInjection/PhpforceSoapClientExtension.php
@@ -8,9 +8,23 @@
 
 namespace Phpforce\Bundle\SoapClient\DependencyInjection;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class PhpforceSoapClientExtension extends Extension
 {
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $processor = new Processor();
 
+        $configuration = new Configuration();
+
+        $config = $processor->process($configuration->getConfigTree(), $configs);
+
+        $container->setParameter('codemitte_appliance.custom_webservices.vg_product_registration_web.wsdl_location', $config['custom_webservices']['vg_product_registration_web']['wsdl_location']);
+
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+
+        $loader->load('services.xml');
+    }
 } 
