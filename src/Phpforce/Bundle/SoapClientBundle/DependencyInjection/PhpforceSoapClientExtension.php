252d9120eddfb750e0c1367899e0730d45635b84
Johannes Heinen <johannes.heinen@gmail.com>
2013-10-31 14:33:49 +0100

    Fixed bundle autoload path

---
+++ src/Phpforce/Bundle/SoapClientBundle/DependencyInjection/PhpforceSoapClientExtension.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: joshi
+ * Date: 31.10.13
+ * Time: 14:29
+ */
+
+namespace Phpforce\Bundle\SoapClient\DependencyInjection;
+
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class PhpforceSoapClientExtension extends Extension
+{
+
+} 
